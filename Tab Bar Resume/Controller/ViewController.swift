8730de8e3e62ddb323a110a88eb03e3b0062815e
Nik <NikIOS@macbook-air-ivan.local>
2019-03-11 20:05:35 +0700

    Commit night mode

--- Tab Bar Resume/Controller/ViewController.swift
+++ Tab Bar Resume/Controller/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     private let tel = "+79029237735"
     private let email = "qqaqq@ya.ru"
-    private var isNightMode: Bool? {
+    private var isNightMode: Bool = false {
         didSet {
             if nightModeButton != nil {
             setMode()
@@ -21,13 +21,11 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        isNightMode = true
-    }
-    
+
+    //MARK: - ...@IBOutlets
     @IBOutlet weak var nightModeButton: UIBarItem!
+    @IBOutlet weak var resumeTextField: UITextView!
+    
     
     //MARK: ...IBActions
     @IBAction func callButtonPressed(_ sender: UIButton) {
@@ -37,15 +35,25 @@ class ViewController: UIViewController {
         emailMe(to: email)
     }
     @IBAction func nightModeToggle(_ sender: UIBarItem) {
-        isNightMode = !isNightMode!
+        isNightMode = !isNightMode
         setNightModInTabBarView()
-        
+    }
+    @IBAction func ballCaunterAppPressed(){
+        let url = URL(string: "https://itunes.apple.com/ru/app//id1366219288?mt=8")!
+        if UIApplication.shared.canOpenURL(url) {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
     }
     
     //MARK: - ...Dovnload functions
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         if nightModeButton != nil {
+            print("Wiew vill apear")
             setMode()
         }
     }
@@ -76,28 +84,44 @@ class ViewController: UIViewController {
     //MARK: - ...NightModeToggle function
     
     private func setNightModInTabBarView() {
-        tabBarController?.viewControllers?.forEach({ (viewController) in
-            guard let viewController = viewController as? ViewController else {return}
+        
+        tabBarController?.viewControllers?.forEach({ (vc) in
             
-            viewController.isNightMode = isNightMode
+            if let navCont = vc as? UINavigationController {
+                navCont.viewControllers.forEach({ (vc) in
+                    if let viewController = vc as? ViewController {
+                        viewController.isNightMode = isNightMode
+                    }
+                })
+            }
+//
         })
     }
     
     private func setMode() {
-        guard let mode = isNightMode else {return}
-        if mode == false {
-            view.backgroundColor = .white
+//        guard let mode = isNightMode else {return}
+        if isNightMode == false {
+            view.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
             tabBarController?.tabBar.barStyle = .default
-            navigationController?.navigationBar.barTintColor = .white
-            navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)]
+            navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+            navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)]
             nightModeButton.title = "night"
+            if resumeTextField != nil {
+            resumeTextField.textColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
+            resumeTextField.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+            }
+            
 
-        } else if mode != false {
-            view.backgroundColor = .black
+        } else if isNightMode != false {
+            view.backgroundColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
             tabBarController?.tabBar.barStyle = .black
-            navigationController?.navigationBar.barTintColor = .black
-            navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)]
+            navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
+            navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)]
             nightModeButton.title = "light"
+            if resumeTextField != nil {
+                resumeTextField.textColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+                resumeTextField.backgroundColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
+            }
 
         }
     }
